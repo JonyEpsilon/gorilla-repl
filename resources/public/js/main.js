d31ff754a9de8689dd0faaa0d74a5f8dc54f22d8
JonyEpsilon <github@j-star.org>
2014-01-30 21:38:32 +0000

    Handle nREPL sessions better. Fixes #13.
    I think. Truthfully, I still don't really understand what nREPL sessions
    are all about.

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -14,30 +14,36 @@ var app = (function () {
     self.start = function () {
 
         // load the configuration from the server
-        $.get("/config", function (data) {
+        $.get("/config",
+            function (data) {
+                // start the REPL - the app is started in a callback from the repl connection that indicates we are
+                // successfully connected.
+                repl.connect(
+                    function () {
+                        var ws;
+                        if (data["worksheet-data"]) {
+                            var segments = worksheetParser.parse(data["worksheet-data"]);
+                            ws = worksheet();
+                            ws.segments = ko.observableArray(segments);
+                        }
+                        else {
+                            // prepare a skeleton worksheet
+                            ws = worksheet();
+                            ws.segments().push(freeSegment("# Gorilla REPL\n\nWelcome to gorilla ..."));
+                            ws.segments().push(codeSegment(""));
+                        }
+                        var wsWrapper = worksheetWrapper(ws);
+                        self.wrapper = wsWrapper;
 
-            // start the REPL
-            repl.connect();
+                        if (data["worksheet-filename"]) wsWrapper.filename = data["worksheet-filename"];
 
-            var ws;
-            if (data["worksheet-data"]) {
-                var segments = worksheetParser.parse(data["worksheet-data"]);
-                ws = worksheet();
-                ws.segments = ko.observableArray(segments);
-            }
-            else {
-                // prepare a skeleton worksheet
-                ws = worksheet();
-                ws.segments().push(freeSegment("# Gorilla REPL\n\nWelcome to gorilla ..."));
-                ws.segments().push(codeSegment(""));
-            }
-            var wsWrapper = worksheetWrapper(ws);
-            self.wrapper = wsWrapper;
-
-            if (data["worksheet-filename"]) wsWrapper.filename = data["worksheet-filename"];
-
-            ko.applyBindings(wsWrapper);
-        });
+                        ko.applyBindings(wsWrapper);
+                    },
+                    // this function is called if we failed to make a REPL connection. We can't really go any further.
+                    function () {
+                        alert("Failed to make initial connection to nREPL server. Refreshing the page might help.");
+                    });
+            })
     };
 
     // respond to save events
