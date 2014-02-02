c57a56e7de9ed8761daf278f2d49b744890a1976
JonyEpsilon <github@j-star.org>
2014-02-02 21:57:58 +0000

    Re-organise startup code.

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -13,46 +13,41 @@ var app = (function () {
     // default content, and bound to the UI.
     self.start = function () {
 
-        // load the configuration from the server
-        $.get("/config",
-            function (data) {
-                // start the REPL - the app is started in a callback from the repl connection that indicates we are
-                // successfully connected.
-                repl.connect(
-                    function () {
-                        var ws;
-                        if (data["worksheet-data"]) {
-                            var segments = worksheetParser.parse(data["worksheet-data"]);
-                            ws = worksheet();
-                            ws.segments = ko.observableArray(segments);
-                        }
-                        else {
-                            // prepare a skeleton worksheet
-                            ws = worksheet();
-                            ws.segments().push(freeSegment("# Gorilla REPL\n\nWelcome to gorilla ..."));
-                            ws.segments().push(codeSegment(""));
-                        }
-                        var wsWrapper = worksheetWrapper(ws);
-                        self.wrapper = wsWrapper;
+        // start the REPL - the app is started in a callback from the repl connection that indicates we are
+        // successfully connected.
+        repl.connect(
+            function () {
+                // prepare a skeleton worksheet
+                ws = worksheet();
+                ws.segments().push(freeSegment("# Gorilla REPL\n\nWelcome to gorilla ..."));
+                ws.segments().push(codeSegment(""));
+                var wsWrapper = worksheetWrapper(ws);
+                self.wrapper = wsWrapper;
 
-                        if (data["worksheet-filename"]) wsWrapper.filename = data["worksheet-filename"];
-
-                        ko.applyBindings(wsWrapper);
-                    },
-                    // this function is called if we failed to make a REPL connection. We can't really go any further.
-                    function () {
-                        alert("Failed to make initial connection to nREPL server. Refreshing the page might help.");
-                    });
-            })
+                ko.applyBindings(wsWrapper);
+            },
+            // this function is called if we failed to make a REPL connection. We can't really go any further.
+            function () {
+                alert("Failed to make initial connection to nREPL server. Refreshing the page might help.");
+            });
     };
 
-    // respond to save events
+    // ** Application event handlers
+
+    eventBus.on("app:load", function () {
+        console.log("load!");
+//        var segments = worksheetParser.parse(data["worksheet-data"]);
+//        ws = worksheet();
+//        ws.segments = ko.observableArray(segments);
+    });
+
     eventBus.on("app:save", function () {
         $.post("/save", {"worksheet-data": self.wrapper.worksheet().toClojure()});
     });
 
     return self;
-})();
+})
+    ();
 
 // The application entry point
 $(function () {
