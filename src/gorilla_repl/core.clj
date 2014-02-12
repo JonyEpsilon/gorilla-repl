f072193fec4bc2d741e8c7384e75e10fe3c9111b
JonyEpsilon <github@j-star.org>
2014-02-12 16:51:32 +0000

    Check headers, add LICENCE.

--- src/gorilla_repl/core.clj
+++ src/gorilla_repl/core.clj
@@ -1,4 +1,4 @@
-;;;; This file is part of gorilla-repl. Copyright (C) 2014, Jony Hudson.
+;;;; This file is part of gorilla-repl. Copyright (C) 2014-, Jony Hudson.
 ;;;;
 ;;;; gorilla-repl is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
