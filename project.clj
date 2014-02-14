8830ea0fe7057a35ce1d4e45efe40ed4d31b4e19
JonyEpsilon <github@j-star.org>
2014-02-14 17:36:57 +0000

    Adding table code ...
    ... just so that I can remove it!

--- project.clj
+++ project.clj
@@ -14,6 +14,7 @@
                  [org.slf4j/slf4j-api "1.7.5"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [clojure-complete "0.2.3"]
+                 [hiccup "1.0.5"]
                  [gorilla-plot "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot gorilla-repl.core
   :target-path "target/%s"
