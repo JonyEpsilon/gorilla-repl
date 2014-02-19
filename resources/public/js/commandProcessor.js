17aa845a21e76bf40740666c84585e6eee4d7658
JonyEpsilon <github@j-star.org>
2014-02-19 12:17:31 +0000

    Revert last change.

--- resources/public/js/commandProcessor.js
+++ resources/public/js/commandProcessor.js
@@ -83,7 +83,7 @@ commandList = [
         name: "command:worksheet:delete",
         desc: "Delete the highlighted segment.",
         showInMenu: true,
-        kb: combo('g', 'd'),
+        kb: combo('g', 'x'),
         action: function () {
             eventBus.trigger("worksheet:delete");
         }
