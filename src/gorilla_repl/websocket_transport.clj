de2eb582f1618ebac06911a3547aef85639331ff
JonyEpsilon <github@j-star.org>
2014-02-27 21:14:40 +0000

    Take a different tack, seems to work.
    Needs quite a bit of cleanup work though. WIP.

--- src/gorilla_repl/websocket_transport.clj
+++ src/gorilla_repl/websocket_transport.clj
@@ -1,28 +1,27 @@
 (ns gorilla-repl.websocket-transport
   (:require [org.httpkit.server :as server]
+            [clojure.tools.nrepl :as nrepl]
             [clojure.tools.nrepl.server :as nrepl-server]
-            [clojure.tools.nrepl.transport :as transport]
             [cheshire.core :as json]))
+
+(defonce server (nrepl-server/start-server :port 7888))
+
+(defonce conn (nrepl/connect :port 7888))
 
 (defn send-json-over-ws
   [channel data]
-  (println "Sent:" data)
   (server/send! channel (json/generate-string data)))
 
-
-(def handler (nrepl-server/default-handler))
-
 (defn process-message
-  [transport data]
-  (println "Received:" data)
-  (let [parsed-message (json/parse-string data true)]
-    (nrepl-server/handle* parsed-message handler transport)))
-
+  [channel data]
+  (let [parsed-message (json/parse-string data true)
+        client (nrepl/client conn 1000)
+        replies (nrepl/message client parsed-message)]
+    (doall (map (partial send-json-over-ws channel) replies))))
 
 (defn ring-handler
   [request]
   (server/with-channel
     request
     channel
-    (let [transport (transport/fn-transport nil (partial send-json-over-ws channel))]
-      (server/on-receive channel (partial process-message transport)))))
+    (server/on-receive channel (partial process-message channel))))
