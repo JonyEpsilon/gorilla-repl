08fde009d7b81840fc7d8babd5c965b6e683f809
JonyEpsilon <github@j-star.org>
2014-08-31 17:30:38 +0100

    Get rid of test data.

--- resources/public/js/docViewer.js
+++ resources/public/js/docViewer.js
@@ -8,8 +8,6 @@ var docViewer = function () {
 
     var self = {};
 
-    var test = "clojure.core/map\n ([f coll] [f c1 c2] [f c1 c2 c3] [f c1 c2 c3 & colls])\nReturns a lazy sequence consisting of the result of applying f to the\nset of first items of each coll, followed by applying f to the set\nof second items in each coll, until any one of the colls is\nexhausted.  Any remaining items in other colls are ignored. Function\nf should accept number-of-colls arguments.";
-
     self.shown = ko.observable(false);
     self.doc = ko.observable("");
     self.show = function () {
