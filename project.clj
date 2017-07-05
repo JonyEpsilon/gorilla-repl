ed172d3ac1000f6e44ca50f55760afc3a5ba37ac
Elise Huard <elise@mastodonc.com>
2017-07-05 10:18:55 +0100

    upgrade clojure and libraries

--- project.clj
+++ project.clj
@@ -2,22 +2,21 @@
 ;;;;
 ;;;; gorilla-repl is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
-(defproject gorilla-repl "0.4.0"
+(defproject gorilla-repl "0.4.1"
   :description "A rich REPL for Clojure in the notebook style."
   :url "https://github.com/JonyEpsilon/gorilla-repl"
   :license {:name "MIT"}
-  :dependencies ^:replace [[org.clojure/clojure "1.6.0"]
-                           [http-kit "2.1.18"]
-                           [ring/ring-json "0.4.0"]
-                           [cheshire "5.3.1"]
-                           [compojure "1.1.8"]
-                           [org.slf4j/slf4j-api "1.7.7"]
-                           [ch.qos.logback/logback-classic "1.1.2"]
+  :dependencies ^:replace [[org.clojure/clojure "1.9.0-alpha16"]
+                           [http-kit "2.2.0" :exclusions [ring/ring-core]]
+                           [ring/ring-json "0.5.0-beta1" :exclusions [org.clojure/clojure]]
+                           [cheshire "5.7.1"]
+                           [compojure "1.6.0" :exclusions [ring/ring-core ring/ring-json]]
+                           [org.slf4j/slf4j-api "1.7.25"]
+                           [ch.qos.logback/logback-classic "1.2.3"]
                            [gorilla-renderable "2.0.0"]
-                           [gorilla-plot "0.1.4"]
-                           [javax.servlet/servlet-api "2.5"]
+                           [gorilla-plot "0.1.4" :exclusions [org.clojure/clojure]]
                            [grimradical/clj-semver "0.2.0" :exclusions [org.clojure/clojure]]
-                           [cider/cider-nrepl "0.10.2"]
+                           [cider/cider-nrepl "0.14.0" :exclusions [org.clojure/clojure]]
                            [org.clojure/tools.nrepl "0.2.12"]]
   :main ^:skip-aot gorilla-repl.core
   :target-path "target/%s")
