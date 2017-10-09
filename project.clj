d893dee304e0c1f549eeb2eb718d264d63aeaf94
Elise Huard <elise@mastodonc.com>
2017-10-09 14:17:49 +0100

    with newest clojure beta version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :description "A rich REPL for Clojure in the notebook style."
   :url "https://github.com/JonyEpsilon/gorilla-repl"
   :license {:name "MIT"}
-  :dependencies ^:replace [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies ^:replace [[org.clojure/clojure "1.9.0-beta2"]
                            [http-kit "2.2.0" :exclusions [ring/ring-core]]
                            [ring/ring-json "0.5.0-beta1" :exclusions [org.clojure/clojure]]
                            [cheshire "5.7.1"]
@@ -16,7 +16,7 @@
                            [gorilla-renderable "2.0.0"]
                            [gorilla-plot "0.1.4" :exclusions [org.clojure/clojure]]
                            [grimradical/clj-semver "0.2.0" :exclusions [org.clojure/clojure]]
-                           [cider/cider-nrepl "0.14.0" :exclusions [org.clojure/clojure]]
-                           [org.clojure/tools.nrepl "0.2.12"]]
+                           [cider/cider-nrepl "0.15.1" :exclusions [org.clojure/clojure]]
+                           [org.clojure/tools.nrepl "0.2.13"]]
   :main ^:skip-aot gorilla-repl.core
   :target-path "target/%s")
